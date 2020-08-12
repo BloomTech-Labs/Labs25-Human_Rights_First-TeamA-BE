5d6ef3e3c9558dd2e22eafff73d7b4060b0de3db
Gordon Caister <gordoncaister@gmail.com>
2020-08-12 15:11:41 -0500

    Created incidents router and fetched sample incidents from sample_incidents.js

---
+++ api/incidents/incidentsRouter.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const SampleIncidents = require('../../data/sample_incidents');
+
+router.get('/', function (req, res) {
+  res.status(200).json({ SampleIncidents });
+});
+
+module.exports = router;
---
+++ data/sample_incidents.js
@@ -0,0 +1,39 @@
+const allIncidents = [
+  {
+    id: 1,
+    location: 'Portland, OR',
+    longitude: '-122.676483',
+    latitude: '45.523064',
+    evidence: [
+      'https://twitter.com/ACLU/status/1292992629647515648',
+      'https://www.reddit.com/r/Portland/comments/i4ggqx/all_gas_no_brakes_portland_protest/',
+    ],
+    title: 'Portland, OR',
+    date: 'Aug 10, 2020',
+  },
+  {
+    id: 2,
+    location: 'New York, NY',
+    longitude: '-73.935242',
+    latitude: '40.730610',
+    evidence: [
+      'https://twitter.com/AliWatkins/status/1267275794822414344',
+      'https://www.nytimes.com/interactive/2020/07/14/nyregion/nypd-george-floyd-protests.html',
+    ],
+    title: 'New York, NY',
+    date: 'May 31, 2020',
+  },
+  {
+    id: 3,
+    location: 'Chicago, IL',
+    longitude: '-87.623177',
+    latitude: '41.881832',
+    evidence: [
+      'https://twitter.com/may20p/status/1292848915855872006',
+      'https://www.nbcnews.com/news/us-news/george-floyd-s-death-painful-reminder-chicago-s-history-police-n1224076',
+    ],
+    title: 'Chicago, IL',
+    date: 'May 31, 2020',
+  },
+];
+module.exports = allIncidents;
